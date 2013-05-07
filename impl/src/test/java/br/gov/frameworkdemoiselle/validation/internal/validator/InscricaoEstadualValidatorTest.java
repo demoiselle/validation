1b97709893ff2f2a09b6dba1c5b57940ba7782e2
Dancovich <danilo.viana@serpro.gov.br>
2013-05-07 13:32:47 -0300

    Limpeza de código

--- impl/src/test/java/br/gov/frameworkdemoiselle/validation/internal/validator/InscricaoEstadualValidatorTest.java
+++ impl/src/test/java/br/gov/frameworkdemoiselle/validation/internal/validator/InscricaoEstadualValidatorTest.java
@@ -36,18 +36,11 @@
  */
 package br.gov.frameworkdemoiselle.validation.internal.validator;
 
-import junit.framework.Assert;
-
-import org.alfredlibrary.utilitarios.inscricaoestadual.InscricaoEstadual.PadraoInscricaoEstadual;
 import org.alfredlibrary.validadores.InscricaoEstadual;
-import org.easymock.EasyMock;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.powermock.api.easymock.PowerMock;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
-
-import br.gov.frameworkdemoiselle.validation.PatternInscricaoEstadual;
 
 @RunWith(PowerMockRunner.class)
 @PrepareForTest(InscricaoEstadual.class)
